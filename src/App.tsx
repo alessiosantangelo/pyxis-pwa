212379326fa4708fe20109fe370f8fc00828d81b
Alessio Santangelo <alessio.santangelo@prima.it>
2023-02-23 15:50:24 +0100

    refactor: revisited app content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import  './images/hero.jpg';
+
 import { Button, Text, Title } from '@pyxis/react'
 import React, {useEffect, useState} from 'react';
 
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 import styles from './App.module.scss';
 import useAddToHomescreenPrompt from './hooks/useBeforeInstallPrompt'
 
@@ -20,43 +22,37 @@ const App = ():React.ReactElement =>  {
     <main className="container container-responsive align-content-center">
       <header className={styles.header}>
         <img className={styles.logoWrapper} src={logo} alt="This is our logo"/>
-        
-        <ul className={styles.menu}>
-          <li className={styles.menuItem}><a href="/" className="link">Home</a></li>
-          <li className={styles.menuItem}><a href="/about" className="link">About</a></li>
-        </ul>
       </header>
-      <div className={styles.heading}>
-        <h1><Title size="xl" weight="bold" className="c-brand-base">Prima Insurance</Title></h1>
-        <h2><Title size="m" className="c-neutral95">Insurance, as you've never experienced it before</Title></h2>
-        <div className="margin-v-xl">
-          <Text>This is a Progressive Web application.</Text>
+      <div className={styles.headingWrapper}>
+        <div className={styles.heading}>
+          <h1><Title size="xl" weight="bold" className="c-neutral-95">Prima Insurance</Title></h1>
         </div>
       </div>
 
-      <div className={styles.installPromptWrapper}>
-        <Title size="m" className="c-neutral95">How to install our PWA on Chrome</Title>
-        <br/>
-        {showPrompt && <Button variant="primary" size="l" className="margin-v-l" onClick={handlePrompt}>Install our App!</Button>}
-        {showPrompt && (
-          <><br/><Text size="m" className="c-neutral95">By clicking this button you accept to install our Progressive Web App.</Text></>
-          )
-        }
-      </div>
+      <section className="margin-v-3xl padding-v-3xl">
+        <Text className="c-neutral-base">This is a Progressive Web application with 1MB background image, just to prove that caching works.</Text>
+      </section>
+    
+      <section className={styles.installPromptWrapper}>
+        <Title size="m" weight="bold" className="c-brand-base">How to install our PWA on Chrome</Title>
+        <div className={styles.installPromptContentWrapper}>
+          <Text size="m" className="c-neutral-95">
+            If your browser supports PWA install, you should see a button appear below or be prompted natively by your Browser in the top-right corner of the address bar.<br/>
+          </Text>
+          {showPrompt && <Button variant="primary" size="l" className="margin-v-l" onClick={handlePrompt}>Install our App!</Button>}  
+        </div>
+      </section>
 
-      <div className={styles.addToHomeScreenWrapper}>
-        <Title size="m" className="c-neutral95">How to install our PWA on Safari & Firefox</Title>
-        <br/>
-        <Text size="m" className="c-neutral95">Safari & Firefox support is &nbsp;
-          <a target="_blank" href="https://caniuse.com/?search=PWA" className="link" rel="noreferrer">under development.</a><br />
-          You can add this application to your Homescreen to use it like a native app.
-        </Text>
-        <img className={styles.addToHomeScreenImage} src="add-to-homescreen.jpg" alt="Add to homescreen example" />
-      </div>
-
-      <footer className="bg-">
-        
-      </footer>
+      <section className={styles.addToHomeScreenWrapper}>
+        <Title size="m" weight="bold" className="c-neutral-125">How to install our PWA on Safari & Firefox</Title>
+        <div className={styles.addToHomeScreenContentWrapper}>
+          <Text size="m" className="c-neutral-125">Safari & Firefox support is &nbsp;
+            <a target="_blank" href="https://caniuse.com/?search=PWA" className="link" rel="noreferrer">under development.</a><br />
+            You can add this application to your Homescreen to use it like a native app.
+          </Text>
+          <img className={styles.addToHomeScreenImage} src="add-to-homescreen.jpg" alt="Add to homescreen example" />
+        </div>
+      </section>
     </main>
 )};
 
